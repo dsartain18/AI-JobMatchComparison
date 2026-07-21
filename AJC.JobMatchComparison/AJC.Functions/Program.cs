674beacedd3737bd0a5b98584fa1dbd65dd6ccd7
Don Sartain <dsartain18@gmail.com>
2026-07-21 14:46:41 -0500

    Added appsettings file and database project

--- AJC.JobMatchComparison/AJC.Functions/Program.cs
+++ AJC.JobMatchComparison/AJC.Functions/Program.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        // Add appsettings.json (optional, reload on change)
+        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+    })
     .ConfigureFunctionsWorkerDefaults()
     .Build();
 
